d49900831875398d2b4ea9b6988c6afe04cb1b09
Bart Hakvoort <hakvoort@src.gnome.org>
2004-09-23 15:31:00 +0000

    stripped i18n.h a bit

--- include/i18n.h
+++ include/i18n.h
@@ -6,22 +6,8 @@
 #endif /* HAVE_CONFIG_H */
 
 #ifdef ENABLE_NLS
-#include <libintl.h>
+#include <libintl.h> 
 #define _(String) gettext(String)
-#ifdef gettext_noop
-#define N_(String) gettext_noop(String)
-#else
-#define N_(String) (String)
-#endif
-#else /* NLS is disabled */
-#define _(String) (String)
-#define N_(String) (String)
-#define textdomain(String) (String)
-#define gettext(String) (String)
-#define dgettext(Domain,String) (String)
-#define dcgettext(Domain,String,Type) (String)
-#define bindtextdomain(Domain,Directory) (Domain) 
-#define bind_textdomain_codeset(Domain,Codeset) (Codeset) 
 #endif /* ENABLE_NLS */
 
 #endif /* I18N */
