7ffc4ac0ad1cc6b421964f7405bc0464680631f8
Bart Hakvoort <hakvoort@src.gnome.org>
2004-09-26 19:36:02 +0000

    removed 'documented by' tab and fixed alignments

--- src/Dialog_About.cc
+++ src/Dialog_About.cc
@@ -46,14 +46,11 @@ Dialog_About::Dialog_About()
 	label_temp -> set_selectable( true ) ;
 	this ->get_vbox()->pack_start( *label_temp ,Gtk::PACK_SHRINK );
 	
-	label_temp = manage( new Gtk::Label() ) ;
-	label_temp -> set_text( "\t\t" ) ;
-	
 	button_credits.add_pixlabel( "/usr/share/icons/hicolor/16x16/stock/generic/stock_about.png", "Credits", 0, 0.5 ) ;
 	button_credits.signal_clicked() .connect( sigc::mem_fun( this, &Dialog_About::Show_Credits ) ) ;
 	
+	this ->get_action_area() ->set_layout( Gtk::BUTTONBOX_EDGE   ) ;
 	this ->get_action_area() ->pack_start( button_credits ) ;
-	this ->get_action_area() ->pack_start( *label_temp ) ;
 	this ->add_button( Gtk::Stock::CLOSE, Gtk::RESPONSE_CLOSE );
 	
 	this ->show_all_children() ;
@@ -69,7 +66,7 @@ void Dialog_About::Show_Credits()
 		
 	//written by
 	vbox_written .set_border_width( 5 ) ;
-	label_writers .set_text( "Bart Hakvoort (gparted@users.sf.net)");
+	label_writers .set_text( "Bart Hakvoort <gparted@users.sf.net>");
 	label_writers .set_alignment( Gtk::ALIGN_LEFT ) ;
 	vbox_written .pack_start( label_writers, Gtk::PACK_SHRINK ) ;
 	notebook_credits .set_size_request( -1, 200 ) ;
@@ -78,7 +75,7 @@ void Dialog_About::Show_Credits()
 	
 	//documented by
 	/*TO TRANSLATORS: tablabel in aboutdialog */
-	notebook_credits .append_page( vbox_documented, _("Documented by") ) ;
+//	notebook_credits .append_page( vbox_documented, _("Documented by") ) ;
 	
 	//translated by
 	/*TO TRANSLATORS: your name(s) here please, if there are more translators put newlines (\n) between the names */
