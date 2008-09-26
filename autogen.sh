a6f93b9e8b4ec2c7ded1970f52c286e4fbb71fc9
Curtis Gedak <gedakc@src.gnome.org>
2008-09-26 21:33:08 +0000

    Added check for gnome-doc-utils.make
    svn path=/trunk/; revision=918

--- autogen.sh
+++ autogen.sh
@@ -11,4 +11,18 @@ which gnome-autogen.sh || {
     exit 1
 }
 
+# Check for GNOME-DOC-UTILS
+GDUMAKE="gnome-doc-utils.make"
+GDUMAKEFULLPATH=`locate $GDUMAKE | grep -m 1 "gnome-doc-utils/"`
+if test "x${GDUMAKEFULLPATH}" = "x" ; then 
+	echo "Cannot find file: $GDUMAKE"
+	echo "You need to install gnome-doc-utils from the GNOME CVS"
+	exit 1
+fi
+# Ensure a copy of gnome-doc-utils.make exists in the top source directory
+test -e $srcdir/$GDUMAKE || {
+	echo "Copying $GDUMAKEFULLPATH to $srcdir"
+	cp -p $GDUMAKEFULLPATH $srcdir
+}
+
 REQUIRED_AUTOMAKE_VERSION=1.9 USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
