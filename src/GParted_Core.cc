a8a035cbae34ff120184a3776eab86397ffe90da
Curtis Gedak <gedakc@src.gnome.org>
2008-10-30 19:41:51 +0000

    Removed ellipses from status messages in set_devices()
    svn path=/trunk/; revision=946

--- src/GParted_Core.cc
+++ src/GParted_Core.cc
@@ -171,8 +171,8 @@ void GParted_Core::set_devices( std::vector<Device> & devices )
     			{
     				//try to have libparted detect the device and add it to the list
     				device = "/dev/" + device;
-    				/*TO TRANSLATORS: looks like Scanning /dev/sda ... */ 
-    				set_thread_status_message( String::ucompose ( _("Scanning %1 ..."), device ) ) ;
+    				/*TO TRANSLATORS: looks like Scanning /dev/sda */ 
+    				set_thread_status_message( String::ucompose ( _("Scanning %1"), device ) ) ;
     				ped_device_get( device .c_str() ) ;
     			}
     		}
@@ -219,8 +219,8 @@ void GParted_Core::set_devices( std::vector<Device> & devices )
 						continue;
 					if ( dm_majors .find( major( st.st_rdev ) ) != dm_majors .end() )
 						//TODO avoid probing partition nodes for dmraid devices
-	    				/*TO TRANSLATORS: looks like Scanning /dev/sda ... */ 
-	    				set_thread_status_message( String::ucompose ( _("Scanning %1 ..."), mapper_name ) ) ;
+	    				/*TO TRANSLATORS: looks like Scanning /dev/sda */ 
+	    				set_thread_status_message( String::ucompose ( _("Scanning %1"), mapper_name ) ) ;
 						ped_device_get( mapper_name .c_str() ) ;
 				}
 				closedir( mapper_dir ) ;
@@ -239,8 +239,8 @@ void GParted_Core::set_devices( std::vector<Device> & devices )
 			char * buf = static_cast<char *>( malloc( lp_device ->sector_size ) ) ;
 			if ( buf )
 			{
-				/*TO TRANSLATORS: looks like Reading /dev/sda ... */ 
-				set_thread_status_message( String::ucompose ( _("Reading %1 ..."), lp_device ->path ) ) ;
+				/*TO TRANSLATORS: looks like Reading /dev/sda */ 
+				set_thread_status_message( String::ucompose ( _("Reading %1"), lp_device ->path ) ) ;
 				if ( ped_device_open( lp_device ) )
 				{
 					if ( ped_device_read( lp_device, buf, 0, 1 ) )
@@ -259,8 +259,8 @@ void GParted_Core::set_devices( std::vector<Device> & devices )
 
 	for ( unsigned int t = 0 ; t < device_paths .size() ; t++ ) 
 	{
-		/*TO TRANSLATORS: looks like Scanning /dev/sda for partitions ... */ 
-		set_thread_status_message( String::ucompose ( _("Scanning %1 for partitions ..."), device_paths[ t ] ) ) ;
+		/*TO TRANSLATORS: looks like Scanning /dev/sda for partitions*/ 
+		set_thread_status_message( String::ucompose ( _("Scanning %1 for partitions"), device_paths[ t ] ) ) ;
 		if ( open_device_and_disk( device_paths[ t ], false ) )
 		{
 			temp_device .Reset() ;
