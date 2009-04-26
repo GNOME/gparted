5112e4181581d95d02d5c041ac63f05ddb9d45af
Curtis Gedak <gedakc@gmail.com>
2009-04-26 15:49:30 -0600

    Update copyright year for FS_Info class

--- src/FS_Info.cc
+++ src/FS_Info.cc
@@ -1,4 +1,4 @@
-/* Copyright (C) 2008 Curtis Gedak
+/* Copyright (C) 2008, 2009 Curtis Gedak
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
