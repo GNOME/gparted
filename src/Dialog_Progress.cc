7c5b5edaef865652225c420946595518419ea614
Curtis Gedak <gedakc@gmail.com>
2012-01-09 19:22:48 -0700

    Reduce graphic processing requirement for pulse bar
    Increase sleep time to decrease pulse bar update frequency.
    
    Debian Bug 499193 - gparted: 100% cpu usage
    
    Debian Bug 519764 - gparted: Lots of animation makes use over SSH
                        X-tunnel slow

--- src/Dialog_Progress.cc
+++ src/Dialog_Progress.cc
@@ -222,7 +222,7 @@ void Dialog_Progress::on_signal_show()
 			while ( Gtk::Main::events_pending() )
 				Gtk::Main::iteration();
 			gdk_threads_leave();
-			usleep( 10000 ) ;
+			usleep( 100000 ) ;
 			gdk_threads_enter();
 		}
 
