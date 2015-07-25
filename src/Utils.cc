4160ae5018192c45195dc2c023066a58f311263f
Mike Fleetwood <mike.fleetwood@googlemail.com>
2015-07-25 09:38:28 +0100

    Update name of the btrfs file system specific package (#753436)
    In Fedora bug report:
        Bug 1176108 - Warning shown on BTRFS partition because of missing btrfs-tools package
        https://bugzilla.redhat.com/show_bug.cgi?id=1176108#c0
    The user said:
        However there is no btrfs-tools package in the standard Fedora repo.
        There is a btrfs-progs package, which is already installed.  It's
        unclear whether this is a real error or simply a mismatched package
        name.
    
    The upstream software is named btrfs-progs.  Arch Linux / CentOS /
    Fedora / Slackware use the upstream name.  Debian / Ubuntu name it
    btrfs-tools and openSUSE calls it btrfsprogs (no dash).
    
    Rename the needed software to:
    
        btrfs-progs / btrfs-tools
    
    Upstream name first separated by slash from alternative names
    distributions use.
    
    NOTE:
    This slightly increases the width of the File System Support dialog on
    my CentOS 6 desktop with default fonts, from 655 to 676 pixels.  Still
    well within the 800 wide target and still narrower that the main window.
    
    Bug 753436 - Update documentation of GParted software dependencies

--- src/Utils.cc
+++ src/Utils.cc
@@ -321,7 +321,7 @@ Glib::ustring Utils::get_filesystem_software( FILESYSTEM filesystem )
 {
 	switch( filesystem )
 	{
-		case FS_BTRFS       : return "btrfs-tools" ;
+		case FS_BTRFS       : return "btrfs-progs / btrfs-tools" ;
 		case FS_EXT2        : return "e2fsprogs" ;
 		case FS_EXT3        : return "e2fsprogs" ;
 		case FS_EXT4        : return "e2fsprogs v1.41+" ;
