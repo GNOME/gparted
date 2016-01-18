0ec726111ddda7ac4c034b7fa10281a4537f8f68
Curtis Gedak <gedakc@gmail.com>
2016-01-18 09:28:01 -0700

    Update copyright year

--- src/Win_GParted.cc
+++ src/Win_GParted.cc
@@ -1603,8 +1603,8 @@ void Win_GParted::menu_help_about()
 	dialog .set_comments( _( "GNOME Partition Editor" ) ) ;
 	std::string names ;
 	names =    "Copyright © 2004-2006 Bart Hakvoort" ;
-	names += "\nCopyright © 2008-2015 Curtis Gedak" ;
-	names += "\nCopyright © 2011-2015 Mike Fleetwood" ;
+	names += "\nCopyright © 2008-2016 Curtis Gedak" ;
+	names += "\nCopyright © 2011-2016 Mike Fleetwood" ;
 	dialog .set_copyright( names ) ;
 
 	//authors
