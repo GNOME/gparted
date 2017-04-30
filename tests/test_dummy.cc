dceb293f15e60ee6a932499b6c63b2a6fb84f38a
Mike Fleetwood <mike.fleetwood@googlemail.com>
2017-04-30 12:34:37 +0100

    Add unit test suites with initial successful dummy test (#781978)
    Now 'make check' will additionally build and run the test suites in the
    ./tests directory.  Add initial always successful dummy test suite.
    This is done using Automake support for testing.
    
        Automake Manual, 15 Support for test suites
        https://www.gnu.org/software/automake/manual/automake.html#Tests
    
    ./tests/Makefile.am takes some influence from the same file in the
    minimal-gtest-autotools template project.
        https://github.com/octol/minimal-gtest-autotools/blob/654848ec0164a80f851fa80b8355099f35c025a2/tests/Makefile.am
    
    Bug 781978 - Add Google Test C++ test framework

---
+++ tests/test_dummy.cc
@@ -0,0 +1,14 @@
+/* Copyright (C) 2017 Mike Fleetwood
+ *
+ *  Copying and distribution of this file, with or without modification,
+ *  are permitted in any medium without royalty provided the copyright
+ *  notice and this notice are preserved.  This file is offered as-is,
+ *  without any warranty.
+ */
+
+#include "gtest/gtest.h"
+
+TEST( DummyTest, Success )
+{
+	EXPECT_TRUE( true );
+}
