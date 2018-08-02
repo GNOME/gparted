f55d64354e47c45b4a40f384c012234fb81291cf
Luca Bacci <luca.bacci982@gmail.com>
2018-08-02 09:24:41 +0200

    prepare-for-gtk3: prepare for removal of Gtk::MenuShell::items() in gtkmm3
    use a map to access menu items
    
    in Gtkmm3 Gtk::MenuShell doesn't provide anymore the STL-like container Gtk::MenuShell::items().
    While we could use Gtk::Container functionality of Gtk::MenuShell it is error prone to
    access items by index, and also requires many downcasts from Gtk::Widget to Gtk::MenuItem,
    which is ugly.
    Instead of keeping track of the indexes we keep a map of menu items
    
    reference:
    https://developer.gnome.org/gtkmm/stable/classGtk_1_1MenuShell.html

--- src/HBoxOperations.cc
+++ src/HBoxOperations.cc
@@ -41,20 +41,27 @@ HBoxOperations::HBoxOperations()
 	pack_start( scrollwindow, Gtk::PACK_EXPAND_WIDGET );
 
 	//create popupmenu
-	menu_popup .append( *manage( new GParted::Menu_Helpers::ImageMenuElem(
+	Gtk::MenuItem *item;
+	item = manage( new GParted::Menu_Helpers::ImageMenuElem(
 		_("_Undo Last Operation"), 
 		* manage( new Gtk::Image( Gtk::Stock::UNDO, Gtk::ICON_SIZE_MENU ) ), 
-		sigc::mem_fun(*this, &HBoxOperations::on_undo) ) ) );
+		sigc::mem_fun(*this, &HBoxOperations::on_undo) ) );
+	menu_popup .append( *item );
+	menu_popup_items[0] = item;
 
-	menu_popup .append( *manage( new GParted::Menu_Helpers::ImageMenuElem(
+	item = manage( new GParted::Menu_Helpers::ImageMenuElem(
 		_("_Clear All Operations"), 
 		* manage( new Gtk::Image( Gtk::Stock::CLEAR, Gtk::ICON_SIZE_MENU ) ), 
-		sigc::mem_fun(*this, &HBoxOperations::on_clear) ) ) );
+		sigc::mem_fun(*this, &HBoxOperations::on_clear) ) );
+	menu_popup .append( *item );
+	menu_popup_items[1] = item;
 
-	menu_popup .append( *manage( new GParted::Menu_Helpers::ImageMenuElem(
+	item = manage( new GParted::Menu_Helpers::ImageMenuElem(
 		_("_Apply All Operations"), 
 		* manage( new Gtk::Image( Gtk::Stock::APPLY, Gtk::ICON_SIZE_MENU ) ), 
-		sigc::mem_fun(*this, &HBoxOperations::on_apply) ) ) );
+		sigc::mem_fun(*this, &HBoxOperations::on_apply) ) );
+	menu_popup .append( *item );
+	menu_popup_items[2] = item;
 
 	menu_popup .append( *manage( new GParted::Menu_Helpers::SeparatorElem() ) );
 	menu_popup .append( *manage( new GParted::Menu_Helpers::StockMenuElem(
@@ -89,9 +96,9 @@ bool HBoxOperations::on_signal_button_press_event( GdkEventButton * event )
 	//right-click
 	if ( event ->button == 3 )
 	{
-		menu_popup .items()[0] .set_sensitive( liststore_operations ->children() .size() > 0 ) ;
-		menu_popup .items()[1] .set_sensitive( liststore_operations ->children() .size() > 0 ) ;
-		menu_popup .items()[2] .set_sensitive( liststore_operations ->children() .size() > 0 ) ;
+		menu_popup_items[0] ->set_sensitive( liststore_operations ->children() .size() > 0 ) ;
+		menu_popup_items[1] ->set_sensitive( liststore_operations ->children() .size() > 0 ) ;
+		menu_popup_items[2] ->set_sensitive( liststore_operations ->children() .size() > 0 ) ;
 
 		menu_popup .popup( event ->button, event ->time ) ;
 	}
