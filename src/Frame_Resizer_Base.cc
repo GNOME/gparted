74bb981ed2e342cf5ea4cc50285eb960e6d68426
Luca Bacci <luca.bacci982@gmail.com>
2019-02-20 12:20:20 +0100

    Use Gdk::RGBA (!25)
    The Gdk::RGBA data type was introduced to replace Gdk::Color in
    Gtkmm 3.0 [1], with Gdk::Color being deprecated in Gtkmm 3.10 [2].
    
    With this commit we make the change to Gdk::RGBA data type which is the
    modern replacement to Gdk::Color.  Gdk::RGBA can be used almost as a
    drop in replacement because it keeps most of the Gdk::Color interface.
    
    Also, this commit removes the C Gtk call introduced during the
    port-to-gtk3 patchset by commit:
        53793527668a344fe54ceb61a1afdc39c24c5c45
        repare-for-gtk3: Prepare for removal of Gtk::Widget::modify_fg() (#7)
    
    [1] Gtkmm 3.0.1 NEWS file
        https://gitlab.gnome.org/GNOME/gtkmm/blob/3.0.1/NEWS#L48
            * RGBA replaces Color, though Color still exists because it is
              used by TextView.  We hope to deprecated Color completely in
              gtkmm 3.2.
    
    [2] Gtkmm 3.10.0 NEWS file
        https://gitlab.gnome.org/GNOME/gtkmm/blob/3.10.0/NEWS#L127
            Gdk:
            * Deprecate Color.
    
    Closes !25 - Modern Gtk3 - part 1

--- src/Frame_Resizer_Base.cc
+++ src/Frame_Resizer_Base.cc
@@ -74,12 +74,12 @@ void Frame_Resizer_Base::init()
 	this ->show_all_children();
 }
 
-void Frame_Resizer_Base::set_rgb_partition_color( const Gdk::Color & color )
+void Frame_Resizer_Base::set_rgb_partition_color(const Gdk::RGBA& color)
 {
 	this ->color_partition = color ;
 }
 
-void Frame_Resizer_Base::override_default_rgb_unused_color( const Gdk::Color & color ) 
+void Frame_Resizer_Base::override_default_rgb_unused_color(const Gdk::RGBA& color)
 {
 	this ->color_unused = color ;
 }
@@ -366,29 +366,29 @@ void Frame_Resizer_Base::draw_partition(const Cairo::RefPtr<Cairo::Context>& cr)
 		UNUSED = 0 ;
 
 	// Background color
-	Gdk::Cairo::set_source_color(cr, color_background);
+	Gdk::Cairo::set_source_rgba(cr, color_background);
 	cr->rectangle(0, 0, 536, 50);
 	cr->fill();
 
 	// The two rectangles on each side of the partition
-	Gdk::Cairo::set_source_color(cr, color_arrow_rectangle);
+	Gdk::Cairo::set_source_rgba(cr, color_arrow_rectangle);
 	cr->rectangle(0, 0, 10, 50);
 	cr->fill();
 	cr->rectangle(526, 0, 10, 50);
 	cr->fill();
 
 	// Partition
-	Gdk::Cairo::set_source_color(cr, color_partition);
+	Gdk::Cairo::set_source_rgba(cr, color_partition);
 	cr->rectangle(X_START, 0, X_END - X_START, 50);
 	cr->fill();
 
 	// Used
-	Gdk::Cairo::set_source_color(cr, color_used);
+	Gdk::Cairo::set_source_rgba(cr, color_used);
 	cr->rectangle(X_START + BORDER, BORDER, USED, 34);
 	cr->fill();
 
 	// Unused
-	Gdk::Cairo::set_source_color(cr, color_unused);
+	Gdk::Cairo::set_source_rgba(cr, color_unused);
 	cr->rectangle(X_START + BORDER + USED, BORDER, UNUSED, 34);
 	cr->fill();
 
@@ -414,16 +414,16 @@ void Frame_Resizer_Base::draw_resize_grip(const Cairo::RefPtr<Cairo::Context>& c
 		arrow_points[ 1 ] .set_x( X_END + GRIPPER ) ; 
 		arrow_points[ 2 ] .set_x( X_END )  ;
 	}
-	
+
 	// Attach resize arrows to the partition
-	Gdk::Cairo::set_source_color(cr, color_arrow_rectangle);
+	Gdk::Cairo::set_source_rgba(cr, color_arrow_rectangle);
 	cr->rectangle((arrow_type == ARROW_LEFT ? X_START - GRIPPER : X_END + 1) + 0.5,
 	              5 + 0.5,
 	              9,
 	              40);
 	cr->stroke();
 
-	Gdk::Cairo::set_source_color(cr, color_arrow);
+	Gdk::Cairo::set_source_rgba(cr, color_arrow);
 	cr->move_to(arrow_points[0].get_x(), arrow_points[0].get_y());
 	cr->line_to(arrow_points[1].get_x(), arrow_points[1].get_y());
 	cr->line_to(arrow_points[2].get_x(), arrow_points[2].get_y());
