2878f364a785684aaf3b97fe5c64a42d92348b45
Luca Bacci <luca.bacci982@gmail.com>
2019-02-20 12:20:20 +0100

    modern-gtk3: Use Gdk::RGBA
    The Gdk::Color datatype and all related APIs are deprecated since
    Gtk 3.0.
    
    With this commit we make the change to Gdk::RGBA datatype which is
    the modern replacement to Gdk::Color. Gdk::RGBA can be used almost
    as a drop in replacement because it keeps most of the Gdk::Color
    interface.
    
    Closes !25 - Modern Gtk3 (part 1)

--- src/Dialog_Partition_Resize_Move.cc
+++ src/Dialog_Partition_Resize_Move.cc
@@ -57,9 +57,9 @@ void Dialog_Partition_Resize_Move::set_data( const Partition & selected_partitio
 		Set_Resizer( false ) ;	
 		Resize_Move_Normal( partitions ) ;
 	}
-	
-	//set partition color
-	Gdk::Color partition_color( Utils::get_color( selected_partition.filesystem ) );
+
+	// Set partition color
+	Gdk::RGBA partition_color(Utils::get_color(selected_partition.filesystem));
 	frame_resizer_base->set_rgb_partition_color( partition_color );
 
 	//store the original values
