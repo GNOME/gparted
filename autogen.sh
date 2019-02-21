f576a000e459652aca76140bfcddd31d2b8de453
Mike Fleetwood <mike.fleetwood@googlemail.com>
2019-02-21 16:30:03 +0000

    Remove use of GNOME 2 gnome-doc-utils documentation infrastructure (!24)
    GNOME Goal: Port to New Documentation
    https://wiki.gnome.org/Initiatives/GnomeGoals/NewDocumentationInfrastructure
    
    First part is to stop using gnome-doc-utils to build and install the
    documentation.  Also since updating the OMF catalog was only needed for
    GNOME 2 yelp, use of scrollkeeper is completely removed.
    
    Closes !24 - Port to GNOME 3 yelp-tools documentation infrastructure

--- autogen.sh
+++ autogen.sh
@@ -25,19 +25,4 @@ if ! test -d $srcdir/m4; then
 	mkdir $srcdir/m4
 fi
 
-# Check for GNOME-DOC-UTILS
-GDUMAKE="gnome-doc-utils.make"
-datadir=`pkg-config --variable=datadir gnome-doc-utils`
-GDUMAKEFULLPATH="$datadir/gnome-doc-utils/$GDUMAKE"
-if test "X$datadir" = 'X' || ! test -f "${GDUMAKEFULLPATH}" ; then
-	echo "Cannot find file: $GDUMAKE"
-	echo "You need to install gnome-doc-utils"
-	exit 1
-fi
-# Ensure a copy of gnome-doc-utils.make exists in the top source directory
-test -e $srcdir/$GDUMAKE || {
-	echo "Copying $GDUMAKEFULLPATH to $srcdir"
-	cp -p $GDUMAKEFULLPATH $srcdir
-}
-
-REQUIRED_AUTOMAKE_VERSION=1.9 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
+REQUIRED_AUTOMAKE_VERSION=1.9 . gnome-autogen.sh
