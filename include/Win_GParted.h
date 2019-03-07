bf301ecd1d96efc071c6392852d50b9252e0dbb5
Luca Bacci <luca.bacci982@gmail.com>
2019-03-07 11:44:05 +0100

    modern-gtk3: Use Gtk::Grid for Win_GParted pt2
    Gtk::Table is deprecated since Gtk 3.4.0. Replace with Gtk::Grid.
    
    This commit makes the change for Win_GParted / pt2.
    
    Closes !25 - Modern Gtk3 (part 1)

--- include/Win_GParted.h
+++ include/Win_GParted.h
@@ -35,7 +35,6 @@
 #include <gtkmm/combobox.h>
 #include <gtkmm/progressbar.h>
 #include <gtkmm/window.h>
-#include <gtkmm/table.h>
 
 namespace GParted
 {
@@ -230,7 +229,6 @@ private:
 	Gtk::Statusbar statusbar;
 	Gtk::Image *image ;
 	Gtk::ScrolledWindow *scrollwindow;
-	Gtk::Table *table ;
 	Gtk::ProgressBar pulsebar ;
 	Gtk::TreeRow treerow;
 	
