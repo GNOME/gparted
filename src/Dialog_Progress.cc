4e6c8671d04e0f0ce56ffccb5024102aebe40c59
Mike Fleetwood <mike.fleetwood@googlemail.com>
2019-03-30 12:22:04 +0000

    Report the same information into saved operation details (!34)
    For consistency save the 3 same lines of information into the saved
    operation details.  None of the new 3 lines are subject to translation.
    As this information is really for our benefit when supporting users
    leaving them as English is OK.  Also "GParted" and "Libparted", as
    previously used, are proper nouns so they were never changed as part of
    any language translation.  See with:
        egrep -r '"(GParted|Libparted)"' po/
    
    Closes !34 - Display more version and configuration information

--- src/Dialog_Progress.cc
+++ src/Dialog_Progress.cc
@@ -16,6 +16,7 @@
  */
 
 #include "Dialog_Progress.h"
+#include "GParted_Core.h"
 #include "OperationDetail.h"
 #include "ProgressBar.h"
 
@@ -376,16 +377,11 @@ void Dialog_Progress::on_save()
 			<< "<meta http-equiv='Content-Type' content='text/html;charset=utf-8' />" << std::endl
 			<< "<title>" << _("GParted Details") << "</title>" << std::endl
 			<< "</head>" << std::endl
-			<< "<body>" << std::endl
-			<< "<p>" << _("GParted") << " " << VERSION
-#ifdef USE_LIBPARTED_DMRAID
-			<< " --enable-libparted-dmraid"
-#endif
-#ifdef ENABLE_ONLINE_RESIZE
-			<< " --enable-online-resize"
-#endif
-			<< "</p>" << std::endl
-			<< "<p>" << _("Libparted") << " " << signal_get_libparted_version .emit() << "</p>" << std::endl ;
+			<< "<body>" << std::endl;
+			std::vector<Glib::ustring> lines;
+			Utils::split(GParted_Core::get_version_and_config_string(), lines, "\n");
+			for (unsigned int i = 0; i < lines.size(); i++)
+				out << "<p>" << lines[i] << "</p>" << std::endl;
 
 			//Write out each operation
 			for ( unsigned int t = 0 ; t < operations .size() ; t++ )
