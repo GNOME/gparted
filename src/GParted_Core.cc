c10379263e70770592326c59e7931c9456025013
Mike Fleetwood <mike.fleetwood@googlemail.com>
2024-10-07 18:40:33 +0100

    Remove obsolete conditional ENABLE_LOOP_DELETE_OLD_PTNS_WORKAROUND (!131)
    Now the minimum requirement is for libparted 3.2, workaround for
    libparted 2.0 to 3.0 not informing the kernel to delete old partitions
    when creating a "loop" table is obsolete.  Therefore remove the
    workaround.
    
    Closes !131 - Increase minimum required version of libparted to 3.2

--- src/GParted_Core.cc
+++ src/GParted_Core.cc
@@ -473,18 +473,6 @@ bool GParted_Core::set_disklabel( const Device & device, const Glib::ustring & d
 	// FIXME: Should call file system specific removal actions
 	// (to remove LVM2 PVs before deleting the partitions).
 
-#ifdef ENABLE_LOOP_DELETE_OLD_PTNS_WORKAROUND
-	// When creating a "loop" table with libparted 2.0 to 3.0 inclusive, it doesn't
-	// inform the kernel to delete old partitions so as a consequence blkid's cache
-	// becomes stale and it won't report a file system subsequently created on the
-	// whole disk device.  Create a GPT first to use that code in libparted to delete
-	// any old partitions.  Fixed in parted 3.1 by commit:
-	//     f5c909c0cd50ed52a48dae6d35907dc08b137e88
-	//     libparted: remove has_partitions check to allow loopback partitions
-	if ( disklabel == "loop" )
-		new_disklabel( device_path, "gpt", false );
-#endif
-
 	// Ensure that any previous whole disk device file system can't be recognised by
 	// libparted in preference to the "loop" partition table signature, or by blkid in
 	// preference to any partition table about to be written.
