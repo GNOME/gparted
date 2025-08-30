3abee6042d655b3ed2fde4536575290658f6fd1a
Mike Fleetwood <mike.fleetwood@googlemail.com>
2025-08-30 08:56:12 +0100

    Add missing includes into OperationResizeMove.h and .cc

--- include/OperationResizeMove.h
+++ include/OperationResizeMove.h
@@ -17,9 +17,12 @@
 #ifndef GPARTED_OPERATIONRESIZEMOVE_H
 #define GPARTED_OPERATIONRESIZEMOVE_H
 
+
+#include "Device.h"
 #include "Operation.h"
 #include "Partition.h"
 #include "PartitionVector.h"
+
 
 namespace GParted
 {
