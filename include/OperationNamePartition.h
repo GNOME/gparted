5a9cc4ac1482b4ce3a93c4af2261a08a085e20d8
Mike Fleetwood <mike.fleetwood@googlemail.com>
2025-08-30 08:52:23 +0100

    Add missing includes into OperationNamePartition.h and .cc

--- include/OperationNamePartition.h
+++ include/OperationNamePartition.h
@@ -17,9 +17,12 @@
 #ifndef GPARTED_OPERATIONNAMEPARTITION_H
 #define GPARTED_OPERATIONNAMEPARTITION_H
 
+
+#include "Device.h"
 #include "Operation.h"
 #include "Partition.h"
 #include "PartitionVector.h"
+
 
 namespace GParted
 {
