17445977bd820d796e0b223754724f3ea8d2f02b
Mike Fleetwood <mike.fleetwood@googlemail.com>
2026-01-19 14:59:07 +0000

    Refactor ext2::read_label() into if fail return early pattern
    To follow a code pattern slowly being applied which makes the code
    easier to understand by not having to remember nested conditional
    contexts.

--- src/ext2.cc
+++ src/ext2.cc
@@ -205,23 +205,22 @@ void ext2::set_used_sectors(Partition& partition)
 }
 
 
-void ext2::read_label( Partition & partition )
+void ext2::read_label(Partition& partition)
 {
 	Glib::ustring output;
 	Glib::ustring error;
-	if ( ! Utils::execute_command( "e2label " + Glib::shell_quote( partition.get_path() ),
-	                               output, error, true )                                   )
+	int exit_status = Utils::execute_command("e2label " + Glib::shell_quote(partition.get_path()),
+	                        output, error, true);
+	if (exit_status != 0)
 	{
-		partition.set_filesystem_label( Utils::trim( output ) );
+		if (! output.empty())
+			partition.push_back_message(output);
+		if (! error.empty())
+			partition.push_back_message(error);
+		return;
 	}
-	else
-	{
-		if ( ! output .empty() )
-			partition.push_back_message( output );
-		
-		if ( ! error .empty() )
-			partition.push_back_message( error );
-	}
+
+	partition.set_filesystem_label(Utils::trim(output));
 }
 
 
