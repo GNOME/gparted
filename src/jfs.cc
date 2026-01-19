29eeb0bfb9b340a6c5e7f45b212a6a691569cd02
Mike Fleetwood <mike.fleetwood@googlemail.com>
2026-01-19 15:01:40 +0000

    Refactor jfs::read_label() into if fail return early pattern

--- src/jfs.cc
+++ src/jfs.cc
@@ -175,23 +175,22 @@ void jfs::set_used_sectors( Partition & partition )
 }
 
 
-void jfs::read_label( Partition & partition )
+void jfs::read_label(Partition& partition)
 {
 	Glib::ustring output;
 	Glib::ustring error;
-	if ( ! Utils::execute_command( "jfs_tune -l " + Glib::shell_quote( partition.get_path() ),
-	                               output, error, true )                                       )
+	int exit_status = Utils::execute_command("jfs_tune -l " + Glib::shell_quote(partition.get_path()),
+	                        output, error, true);
+	if (exit_status != 0)
 	{
-		partition.set_filesystem_label( Utils::regexp_label( output, "^Volume label:[\t ]*'(.*)'" ) );
+		if (! output.empty())
+			partition.push_back_message(output);
+		if (! error.empty())
+			partition.push_back_message(error);
+		return;
 	}
-	else
-	{
-		if ( ! output .empty() )
-			partition.push_back_message( output );
-		
-		if ( ! error .empty() )
-			partition.push_back_message( error );
-	}
+
+	partition.set_filesystem_label(Utils::regexp_label(output, "^Volume label:[\t ]*'(.*)'"));
 }
 
 
