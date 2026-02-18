7d6f9b4b9e51415888795b6d4c85abf872b73732
Mike Fleetwood <mike.fleetwood@googlemail.com>
2026-02-18 18:54:11 +0000

    Add missing includes into nilfs2.h and .cc
    nilfs2.h
        #include "OperationDetail.h"  // For OperationDetail
    
    nilfs2.cc
        #include "OperationDetail.h"  // For OperationDetail,
                                      // EXEC_CHECK_STATUS
        #include "Utils.h"            // For FS_NILFS2, KIBIBYTE, MEBIBYTE,
                                      // Sector, Utils::*()
        #include <glibmm/ustring.h>   // For Glib::ustring
        #include <stdio.h>            // For sscanf()

--- include/nilfs2.h
+++ include/nilfs2.h
@@ -18,7 +18,9 @@
 #ifndef GPARTED_NILFS2_H
 #define GPARTED_NILFS2_H
 
+
 #include "FileSystem.h"
+#include "OperationDetail.h"
 #include "Partition.h"
 
 
--- src/nilfs2.cc
+++ src/nilfs2.cc
@@ -14,12 +14,18 @@
  *  along with this program; if not, see <http://www.gnu.org/licenses/>.
  */
 
+
 #include "nilfs2.h"
+
 #include "FileSystem.h"
+#include "OperationDetail.h"
 #include "Partition.h"
+#include "Utils.h"
 
 #include <glibmm/miscutils.h>
 #include <glibmm/shell.h>
+#include <glibmm/ustring.h>
+#include <stdio.h>
 
 
 namespace GParted
