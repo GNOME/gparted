b435bb7c1dc69e17e9f5bf8a96d80ab8b8f32c3c
Mike Fleetwood <mike.fleetwood@googlemail.com>
2026-05-30 16:47:28 +0100

    Add operation detail callback unit tests (!174)
    Add test fixture class to provide callback method helpers for this group
    of tests.
    
    Closes !174 - Add unit tests for OperationDetail and convert to use
                  smart pointers

--- tests/test_OperationDetail.cc
+++ tests/test_OperationDetail.cc
@@ -20,7 +20,11 @@
 #include "Utils.h"
 #include "gtest/gtest.h"
 
+#include <glibmm/main.h>
+#include <glibmm/ustring.h>
 #include <gtkmm/main.h>
+#include <sigc++/bind.h>
+#include <sigc++/signal.h>
 #include <stdio.h>
 
 
@@ -32,7 +36,49 @@ namespace GParted
 static const char* TIME_PATTERN = "(-?[[:digit:]]{2,}:[[:digit:]]{2}:[[:digit:]]{2})";
 
 
-TEST(OperationDetailTest, CreateEmpty)
+class OperationDetailTest : public ::testing::Test
+{
+protected:
+	bool          m_callback_called   = false;
+	Glib::ustring m_callback_treepath;
+
+public:
+	void stream_callback(OperationDetail* od)        { m_callback_called = true; };
+	bool timed_callback(OperationDetail* od)         { m_callback_called = true;
+	                                                   return false;  /* Unregister callback */ };
+	void update_callback(const OperationDetail& od)  { m_callback_called = true;
+	                                                   m_callback_treepath = od.get_treepath(); };
+	void capture_errors_callback(const OperationDetail& od, bool success)
+	                                                 { m_callback_called = true; };
+
+	bool trigger_cancel(OperationDetail& od, bool force)
+	                                                 { od.signal_cancel.emit(force);
+	                                                   return false;  /* Unregister callback */ };
+
+	bool trigger_2_stage_cancel(OperationDetail& od, bool force)
+	{
+		if (force == false)
+		{
+			// Stage 1: non-forced cancel
+			od.signal_cancel.emit(false);
+
+			// Register stage 2 forced cancel
+			Glib::signal_idle().connect(sigc::bind<OperationDetail&, bool>(
+						sigc::mem_fun(*this, &OperationDetailTest::trigger_2_stage_cancel),
+						od,
+						true));
+		}
+		else  // (force == true)
+		{
+			// Stage 2: forced cancel
+			od.signal_cancel.emit(true);
+		}
+		return false;  // Unregister callback
+	}
+};
+
+
+TEST_F(OperationDetailTest, CreateEmpty)
 {
 	// Test creating an empty OperationDetail using default constructor.
 	OperationDetail od;
@@ -44,7 +90,7 @@ TEST(OperationDetailTest, CreateEmpty)
 }
 
 
-TEST(OperationDetailTest, CreateWithDescription)
+TEST_F(OperationDetailTest, CreateWithDescription)
 {
 	// Test creating an OperationDetail passing description using parameterised
 	// constructor.  Status defaults to EXECUTE.
@@ -54,7 +100,7 @@ TEST(OperationDetailTest, CreateWithDescription)
 }
 
 
-TEST(OperationDetailTest, CreateWithDescriptionStatus)
+TEST_F(OperationDetailTest, CreateWithDescriptionStatus)
 {
 	// Test creating an OperationDetail passing description and status using
 	// parameterised constructor.
@@ -64,7 +110,7 @@ TEST(OperationDetailTest, CreateWithDescriptionStatus)
 }
 
 
-TEST(OperationDetailTest, CreateWithDescriptionStatusFont)
+TEST_F(OperationDetailTest, CreateWithDescriptionStatusFont)
 {
 	// Test creating an OperationDetail passing description, status and font using
 	// parameterised constructor.
@@ -74,7 +120,7 @@ TEST(OperationDetailTest, CreateWithDescriptionStatusFont)
 }
 
 
-TEST(OperationDetailTest, ElapseTimeStopWithSuccess)
+TEST_F(OperationDetailTest, ElapseTimeStopWithSuccess)
 {
 	// Test status transition EXECUTE -> SUCCESS records elapsed time.
 	OperationDetail od("ElapseTimeStopWithSuccess", STATUS_EXECUTE);
@@ -89,7 +135,7 @@ TEST(OperationDetailTest, ElapseTimeStopWithSuccess)
 }
 
 
-TEST(OperationDetailTest, ElapseTimeStopWithWarning)
+TEST_F(OperationDetailTest, ElapseTimeStopWithWarning)
 {
 	// Test status transition EXECUTE -> WARNING records elapsed time.
 	OperationDetail od("ElapseTimeStopWithWarning", STATUS_EXECUTE);
@@ -99,7 +145,7 @@ TEST(OperationDetailTest, ElapseTimeStopWithWarning)
 }
 
 
-TEST(OperationDetailTest, ElapseTimeStopWithError)
+TEST_F(OperationDetailTest, ElapseTimeStopWithError)
 {
 	// Test status transition EXECUTE -> WARNING records elapsed time.
 	OperationDetail od("ElapseTimeStopWithError", STATUS_EXECUTE);
@@ -109,7 +155,7 @@ TEST(OperationDetailTest, ElapseTimeStopWithError)
 }
 
 
-TEST(OperationDetailTest, Hierarchy)
+TEST_F(OperationDetailTest, Hierarchy)
 {
 	// Test creating and accessing this OperationDetail tree hierarchy:
 	//     Description              Treepath
@@ -155,7 +201,7 @@ TEST(OperationDetailTest, Hierarchy)
 }
 
 
-TEST(OperationDetailTest, CommandExecuteTrue)
+TEST_F(OperationDetailTest, CommandExecuteTrue)
 {
 	// Test command returning exit status 0 (AKA success).
 	OperationDetail od("CommandExecuteTrue");
@@ -164,7 +210,7 @@ TEST(OperationDetailTest, CommandExecuteTrue)
 }
 
 
-TEST(OperationDetailTest, CommandExecuteFalse)
+TEST_F(OperationDetailTest, CommandExecuteFalse)
 {
 	// Test command returning non-zero exit status (AKA failure).
 	OperationDetail od("CommandExecuteFalse");
@@ -173,7 +219,7 @@ TEST(OperationDetailTest, CommandExecuteFalse)
 }
 
 
-TEST(OperationDetailTest, CommandExecuteExit2)
+TEST_F(OperationDetailTest, CommandExecuteExit2)
 {
 	// Test command returning exit status 2.
 	OperationDetail od("CommandExecuteExit2");
@@ -182,7 +228,7 @@ TEST(OperationDetailTest, CommandExecuteExit2)
 }
 
 
-TEST(OperationDetailTest, CommandExecuteProvideStdin)
+TEST_F(OperationDetailTest, CommandExecuteProvideStdin)
 {
 	// Test passing data on stdin for command to read.
 	OperationDetail od("CommandExecuteProvideStdin");
@@ -198,7 +244,7 @@ TEST(OperationDetailTest, CommandExecuteProvideStdin)
 }
 
 
-TEST(OperationDetailTest, CommandExecuteCaptureStdout)
+TEST_F(OperationDetailTest, CommandExecuteCaptureStdout)
 {
 	// Test capturing command writing to stdout.
 	OperationDetail od("CommandExecuteCaptureStdout");
@@ -214,7 +260,7 @@ TEST(OperationDetailTest, CommandExecuteCaptureStdout)
 }
 
 
-TEST(OperationDetailTest, CommandExecuteCaptureStderr)
+TEST_F(OperationDetailTest, CommandExecuteCaptureStderr)
 {
 	// Test capturing command writing to stderr.
 	OperationDetail od("CommandExecuteCaptureStderr");
@@ -230,7 +276,7 @@ TEST(OperationDetailTest, CommandExecuteCaptureStderr)
 }
 
 
-TEST(OperationDetailTest, CommandExecuteCheckTrue)
+TEST_F(OperationDetailTest, CommandExecuteCheckTrue)
 {
 	// Test checked command returning exit status 0.
 	OperationDetail od("CommandExecuteCheckTrue");
@@ -240,12 +286,202 @@ TEST(OperationDetailTest, CommandExecuteCheckTrue)
 }
 
 
-TEST(OperationDetailTest, CommandExecuteCheckFalse)
+TEST_F(OperationDetailTest, CommandExecuteCheckFalse)
 {
 	// Test check command returning non-zero exit status.
 	OperationDetail od("CommandExecuteCheckFalse");
 	int exit_status = od.execute_command("false", EXEC_CHECK_STATUS);
 	EXPECT_NE(exit_status, 0);
+	EXPECT_EQ(od.get_last_child().get_status(), STATUS_ERROR);
+}
+
+
+TEST_F(OperationDetailTest, CommandExecuteProgressStdoutCallback)
+{
+	// Test reading stdout triggers progress callback to be called.
+	OperationDetail od("CommandExecuteProgressStdoutCallback");
+	EXPECT_FALSE(m_callback_called);
+
+	int exit_status = od.execute_command("echo 'Progress callback triggered from reading standard output'",
+	                        EXEC_PROGRESS_STDOUT,
+	                        static_cast<StreamSlot>(sigc::mem_fun(*this, &OperationDetailTest::stream_callback)));
+	EXPECT_EQ(exit_status, 0);
+	EXPECT_TRUE(m_callback_called);
+}
+
+
+TEST_F(OperationDetailTest, CommandExecuteProgressStderrCallback)
+{
+	// Test reading stderr triggers progress callback to be called.
+	OperationDetail od("CommandExecuteProgressStderrCallback");
+	EXPECT_FALSE(m_callback_called);
+
+	int exit_status = od.execute_command("sh -c -- \"echo 'Progress callback triggered from reading standard error' 1>&2\"",
+	                        EXEC_PROGRESS_STDERR,
+	                        static_cast<StreamSlot>(sigc::mem_fun(*this, &OperationDetailTest::stream_callback)));
+	EXPECT_EQ(exit_status, 0);
+	EXPECT_TRUE(m_callback_called);
+}
+
+
+TEST_F(OperationDetailTest, CommandExecuteProgressTimedCallback)
+{
+	// Test time triggered progress callback is called.
+	// (Is called every 500ms hence sleeping 1 second).
+	OperationDetail od("CommandExecuteProgressTimedCallback");
+	EXPECT_FALSE(m_callback_called);
+
+	int exit_status = od.execute_command("sleep 1",
+	                        EXEC_PROGRESS_TIMED,
+	                        static_cast<TimedSlot>(sigc::mem_fun(*this, &OperationDetailTest::timed_callback)));
+	EXPECT_EQ(exit_status, 0);
+	EXPECT_TRUE(m_callback_called);
+}
+
+
+TEST_F(OperationDetailTest, UpdateCallbackSetDescription)
+{
+	// Test setting description calls update callback.
+	OperationDetail od("UpdateCallbackSetDescription", STATUS_NONE);
+	od.set_treepath("0");  // Non-empty treepath required for update callback to be called.
+	od.signal_update.connect(sigc::mem_fun(*this, &OperationDetailTest::update_callback));
+	EXPECT_FALSE(m_callback_called);
+
+	od.set_description("UpdateCallBackSetDescription: updated");
+	EXPECT_TRUE(m_callback_called);
+}
+
+
+TEST_F(OperationDetailTest, UpdateCallbackSetStatus)
+{
+	// Test setting status calls update callback.
+	OperationDetail od("UpdateCallbackSetStatus", STATUS_NONE);
+	od.set_treepath("0");  // Non-empty treepath required for update callback to be called.
+	od.signal_update.connect(sigc::mem_fun(*this, &OperationDetailTest::update_callback));
+	EXPECT_FALSE(m_callback_called);
+
+	od.set_status(STATUS_SUCCESS);
+	EXPECT_TRUE(m_callback_called);
+}
+
+
+TEST_F(OperationDetailTest, UpdateCallbackAddChild)
+{
+	// Test adding child calls update callback on the child.
+	OperationDetail od("UpdateCallbackAddChild", STATUS_NONE);
+	od.set_treepath("0");  // Non-empty treepath required for update callback to be called.
+	od.signal_update.connect(sigc::mem_fun(*this, &OperationDetailTest::update_callback));
+	EXPECT_FALSE(m_callback_called);
+
+	od.add_child(OperationDetail("UpdateCallBackAddChild: child", STATUS_NONE));
+	EXPECT_TRUE(m_callback_called);
+	EXPECT_STREQ(m_callback_treepath.c_str(), "0:0");
+}
+
+
+TEST_F(OperationDetailTest, SetSuccessCaptureErrorsCallbackTrue)
+{
+	// Test setting success calls capture errors callback and sets status to
+	// STATUS_SUCCESS.
+	OperationDetail od("SetSuccessCaptureErrorsCallbackTrue", STATUS_NONE);
+	od.signal_capture_errors.connect(sigc::mem_fun(*this, &OperationDetailTest::capture_errors_callback));
+	EXPECT_FALSE(m_callback_called);
+
+	od.set_success_and_capture_errors(true);
+	EXPECT_EQ(od.get_status(), STATUS_SUCCESS);
+	EXPECT_TRUE(m_callback_called);
+}
+
+
+TEST_F(OperationDetailTest, SetSuccessCaptureErrorsCallbackFalse)
+{
+	// Test setting success calls capture errors callback and sets status to
+	// STATUS_ERROR.
+	OperationDetail od("SetSuccessCaptureErrorsCallbackFalse", STATUS_NONE);
+	od.signal_capture_errors.connect(sigc::mem_fun(*this, &OperationDetailTest::capture_errors_callback));
+	EXPECT_FALSE(m_callback_called);
+
+	od.set_success_and_capture_errors(false);
+	EXPECT_EQ(od.get_status(), STATUS_ERROR);
+	EXPECT_TRUE(m_callback_called);
+}
+
+
+TEST_F(OperationDetailTest, CancelCallbackSafe)
+{
+	// Test cancelling a cancel safe running command kills it without forcing.
+	// (This is how GParted executes EXEC_CANCEL_SAFE commands and allows them to be
+	// cancelled without forcing the cancel).
+	OperationDetail od("CancelCallbackSafe");
+	od.set_treepath("0");
+
+	// Register trigger_cancel() as a Gtk::Main "idle" task which runs when
+	// execute_command() is waiting for sleep 1 to run.  trigger_cancel() calls
+	// od.signal_cancel.emit(false) to initiate command cancellation.
+	// (In GParted Applying pending operations dialog when [Cancel] is pressed it
+	// calls signal_cancel.emit(false) on the top-level operation detail object).
+	Glib::signal_idle().connect(sigc::bind<OperationDetail&, bool>(
+				sigc::mem_fun(*this, &OperationDetailTest::trigger_cancel),
+				od,
+				false));
+	int exit_status = od.execute_command("sleep 1", EXEC_CANCEL_SAFE|EXEC_CHECK_STATUS);
+	EXPECT_GT(exit_status, 128);  // Killed.  Shell style exit status: 128 + signal number
+	EXPECT_EQ(od.get_last_child().get_status(), STATUS_ERROR);
+}
+
+
+TEST_F(OperationDetailTest, CancelCallbackUnsafeFalse)
+{
+	// Test cancelling a cancel unsafe running command doesn't kill it when not
+	// forced.
+	OperationDetail od("CancelCallbackUnsafeFalse");
+	od.set_treepath("0");
+
+	Glib::signal_idle().connect(sigc::bind<OperationDetail&, bool>(
+				sigc::mem_fun(*this, &OperationDetailTest::trigger_cancel),
+				od,
+				false));
+	int exit_status = od.execute_command("sleep 1", EXEC_CHECK_STATUS);
+	EXPECT_EQ(exit_status, 0);  // Not killed.
+	EXPECT_EQ(od.get_last_child().get_status(), STATUS_SUCCESS);
+}
+
+
+TEST_F(OperationDetailTest, CancelCallbackUnsafeTrue)
+{
+	// Test cancelling a cancel unsafe running command kills it when immediately
+	// forced.
+	// (Note that GParted doesn't do this.  See following test for how GParted does
+	// behave).
+	OperationDetail od("CancelCallbackUnsafeTrue");
+	od.set_treepath("0");
+
+	Glib::signal_idle().connect(sigc::bind<OperationDetail&, bool>(
+				sigc::mem_fun(*this, &OperationDetailTest::trigger_cancel),
+				od,
+				true));
+	int exit_status = od.execute_command("sleep 1", EXEC_CHECK_STATUS);
+	EXPECT_GT(exit_status, 128);  // Killed.  Shell style exit status: 128 + signal number
+	EXPECT_EQ(od.get_last_child().get_status(), STATUS_ERROR);
+}
+
+
+TEST_F(OperationDetailTest, CancelCallbackUnsafe2StageFalseTrue)
+{
+	// Test cancelling a cancel unsafe running command.  First does a non-forced
+	// cancel, followed by a forced cancel.
+	// (This is how GParted behaves.  Press [Cancel] to perform the first non-forced
+	// cancel.  Then after 5 seconds press [Force Cancel] and [Cancel Operation] to
+	// perform the second forced cancel.
+	OperationDetail od("CancelCallbackUnsafe2StageFalseTrue");
+	od.set_treepath("0");
+
+	Glib::signal_idle().connect(sigc::bind<OperationDetail&, bool>(
+				sigc::mem_fun(*this, &OperationDetailTest::trigger_2_stage_cancel),
+				od,
+				false));
+	int exit_status = od.execute_command("sleep 1", EXEC_CHECK_STATUS);
+	EXPECT_GT(exit_status, 128);  // Killed.  Shell style exit status: 128 + signal number
 	EXPECT_EQ(od.get_last_child().get_status(), STATUS_ERROR);
 }
 
