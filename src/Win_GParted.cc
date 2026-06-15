0bc7f1816e768f7e60bd33db33fff4b93d03d0c0
Mike Fleetwood <mike.fleetwood@googlemail.com>
2026-06-15 21:09:30 +0100

    Recognise contribution by Patrick Verner

--- src/Win_GParted.cc
+++ src/Win_GParted.cc
@@ -1979,6 +1979,7 @@ void Win_GParted::menu_help_about()
 	strings .push_back( "Pali Rohár <pali.rohar@gmail.com>" );
 	strings .push_back( "Phillip Susi <psusi@cfl.rr.com>" ) ;
 	strings .push_back( "Antoine Viallon <antoine.viallon@gmail.com>" );
+	strings .push_back( "Patrick Verner <sales@partedmagic.com>" );
 	strings. push_back( "Michael Zimmermann <sigmaepsilon92@gmail.com>" ) ;
 	dialog .set_authors( strings ) ;
 	strings .clear() ;
